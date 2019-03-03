6cef0d82a7b76495c40d86d6d55a3112b39ffdc5
Jeejz <jijo88@gmail.com>
2019-03-04 01:10:25 +0530

    Added messages service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,18 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HEROES } from './heroes.mockdata';
 import { Hero } from './heroes.model';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService:MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.addMessage("Reaching const for data");
     return of(HEROES);
   }
-  // getHeroes(): Hero[] {
-  //   return HEROES;
-  // }
+
+  
+ 
 }
